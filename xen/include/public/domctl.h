fa435eb6218fa3dae944b02041c4aa80e962b68a
Stewart Hildebrand <stewart.hildebrand@amd.com>
2023-10-25 13:08:59 -0400

    xen/vpci: move xen_domctl_createdomain vPCI flag to common
    Both x86 and ARM need a way at domain creation time to specify whether
    the domain needs vPCI emulation. Move the vPCI flag from x86
    xen_domctl_createdomain.arch.emulation_flags to the common
    xen_domctl_createdomain.flags.
    
    Move has_vpci() macro to common header.
    
    Bump XEN_DOMCTL_INTERFACE_VERSION since we're modifying flags inside
    struct xen_domctl_createdomain and xen_arch_domainconfig.
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
    Signed-off-by: Rahul Singh <rahul.singh@arm.com>
    Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com>
    Acked-by: Christian Lindig <christian.lindig@cloud.com>
    ---
    RFC: In light of recent discussions [3], this flag may not need to be
         passed from the toolstack to the hypervisor anymore. We still need
         to track internal to the hypervisor if the domain uses vPCI, but
         the information instead may be passed via the XEN_DOMCTL_assign_device
         hypercall.
    
    v6->v7:
    * move ( vpci && !hvm ) check back to xen/common/domain.c
    * shorten comment in xen/include/public/arch-x86/xen.h
    * fix build in CONFIG_HVM=n case
    
    v5->v6:
    * don't explicitly clear XEN_DOMCTL_CDF_vpci where unnecessary
    * add comment about avoiding re-using bit 10 in emulation_flags
    * don't #define _XEN_DOMCTL_CDF_vpci (underscore-prefixed), only
      introduce single new flag identifier
    * move ( vpci && !hvm ) check to xen/arch/x86/domain.c
    * simplify has_vpci()
    
    v4->v5:
    * move flags_optional change in xen/arch/arm/domain.c to next patch
    * change latter 2 args to emulation_flags_ok() to unsigned int
    * move vpci && !hvm check to common code
    * remove stray spaces in emulation_flags_ok(), and a minor style fixup
    * add CONFIG_HAS_VPCI check into has_vpci()
    * add Christian's A-b (OCaml)
    
    v3->v4:
    * renamed, was:
      ("xen/arm: pci: plumb xen_arch_domainconfig with pci info")
    * reworked: move x86 vPCI flag to common instead of adding another arch
      specific vPCI flag
    * folded ("xen/arm: make has_vpci() depend on d->arch.has_vpci") into
      this patch (retain Signed-off-by's from [1] and [2])
    
    v2->v3:
    * new patch
    
    [1] https://gitlab.com/xen-project/people/bmarquis/xen-arm-poc/-/commit/27be1729ce8128dbe37275ce7946b2fbd2e5a382
    [2] https://github.com/xen-troops/xen/commit/bf12185e6fb2e31db0d8e6ea9ccd8a02abadec17
    [3] https://lists.xenproject.org/archives/html/xen-devel/2023-12/msg00485.html

--- xen/include/public/domctl.h
+++ xen/include/public/domctl.h
@@ -69,8 +69,10 @@ struct xen_domctl_createdomain {
 /* Should we trap guest accesses to unmapped addresses? */
 #define XEN_DOMCTL_CDF_trap_unmapped_accesses  (1U << 8)
 
+/* Should vPCI be enabled for the guest? */
+#define XEN_DOMCTL_CDF_vpci           (1U << 9)
 /* Max XEN_DOMCTL_CDF_* constant.  Used for ABI checking. */
-#define XEN_DOMCTL_CDF_MAX XEN_DOMCTL_CDF_trap_unmapped_accesses
+#define XEN_DOMCTL_CDF_MAX XEN_DOMCTL_CDF_vpci
 
     uint32_t flags;
 
