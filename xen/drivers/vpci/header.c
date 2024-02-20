b88bfc1629200499f7c5e93ec7e7c084d5baecf6
Stefano Stabellini <stefano.stabellini@amd.com>
2024-02-20 15:36:18 -0500

    xen/pci: introduce has_vpci_bridge
    has_vpci_bridge is a macro to check if the domain is a domU or is dom0
    with vPCI (pci-scan=yes) enabled.
    
    Use the macro in drivers/vpci.
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@amd.com>
    Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com>
    ---
    Reviewed-by: Stefano Stabellini <stefano.stabellini@amd.com> # xlnx downstream
    
    * separate definitions for Arm & x86
    * s/\!is_hardware_domain/has_vpci_bridge/ for "vpci/msi: pending bit handler"

--- xen/drivers/vpci/header.c
+++ xen/drivers/vpci/header.c
@@ -603,7 +603,7 @@ static void cf_check cmd_write(
 {
     struct vpci_header *header = data;
 
-    if ( !is_hardware_domain(pdev->domain) )
+    if ( has_vpci_bridge(pdev->domain) )
     {
         const struct vpci *vpci = pdev->vpci;
 
@@ -646,7 +646,7 @@ static void cf_check bar_write(
     struct vpci_bar *bar = data;
     bool hi = false;
 
-    ASSERT(is_hardware_domain(pdev->domain));
+    ASSERT(!has_vpci_bridge(pdev->domain));
 
     if ( bar->type == VPCI_BAR_MEM64_HI )
     {
@@ -823,7 +823,7 @@ int vpci_init_header(struct pci_dev *pdev)
     struct vpci_header *header = &pdev->vpci->header;
     struct vpci_bar *bars = header->bars;
     int rc;
-    bool is_hwdom = is_hardware_domain(pdev->domain);
+    bool is_hwdom = !has_vpci_bridge(pdev->domain);
     uint8_t type;
 
     ASSERT(rw_is_write_locked(&pdev->domain->pci_lock));
