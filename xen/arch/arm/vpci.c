3f57fccacb3601d52417ca623631d1b1f31f0c54
Stefano Stabellini <stefano.stabellini@amd.com>
2024-02-20 15:36:19 -0500

    xen/pci: use has_vpci_bridge in vpci and pci
    Use has_vpci_bridge instead of is_hardware_domain checks in
    vpci_sbdf_from_gpa, domain_vpci_init, domain_vpci_get_num_mmio_handlers,
    pci_get_pdev.
    
    The presence or absence of a bridge no longer corresponds to whether the
    domain is_hardware_domain, so remove the now invalid ASSERTs.
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@amd.com>
    Signed-off-by: Stewart Hildebrand <stewart.hildebrand@amd.com>
    ---
    This was probably true at some point in the past, in an old xlnx_rebase_4.* branch:
    Reviewed-by: Stefano Stabellini <stefano.stabellini@amd.com>

--- xen/arch/arm/vpci.c
+++ xen/arch/arm/vpci.c
@@ -9,12 +9,13 @@
 
 #include <asm/mmio.h>
 
-static pci_sbdf_t vpci_sbdf_from_gpa(const struct pci_host_bridge *bridge,
+static pci_sbdf_t vpci_sbdf_from_gpa(struct domain *d,
+                                     const struct pci_host_bridge *bridge,
                                      paddr_t gpa, bool use_root)
 {
     pci_sbdf_t sbdf;
 
-    if ( bridge )
+    if ( !has_vpci_bridge(d) )
     {
         const struct pci_config_window *cfg = use_root ? bridge->cfg
                                                        : bridge->child_cfg;
@@ -23,7 +24,11 @@ static pci_sbdf_t vpci_sbdf_from_gpa(const struct pci_host_bridge *bridge,
         sbdf.bus += cfg->busn_start;
     }
     else
-        sbdf.sbdf = VPCI_ECAM_BDF(gpa - GUEST_VPCI_ECAM_BASE);
+    {
+        paddr_t start = domain_use_host_layout(d) ? bridge->cfg->phys_addr :
+                                                    GUEST_VPCI_ECAM_BASE;
+        sbdf.sbdf = VPCI_ECAM_BDF(gpa - start);
+    }
 
     return sbdf;
 }
@@ -52,8 +57,8 @@ static int vpci_mmio_read_root(struct vcpu *v, mmio_info_t *info, register_t *r,
                                void *p)
 {
     struct pci_host_bridge *bridge = p;
-    pci_sbdf_t sbdf = vpci_sbdf_from_gpa(bridge, info->gpa, true);
-
+    pci_sbdf_t sbdf = vpci_sbdf_from_gpa(v->domain, bridge, info->gpa, true);
+    
     ASSERT(!bridge == !is_hardware_domain(v->domain));
 
     return vpci_mmio_read(v, info, r, sbdf);
@@ -63,7 +68,7 @@ static int vpci_mmio_read_child(struct vcpu *v, mmio_info_t *info,
                                 register_t *r, void *p)
 {
     struct pci_host_bridge *bridge = p;
-    pci_sbdf_t sbdf = vpci_sbdf_from_gpa(bridge, info->gpa, false);
+    pci_sbdf_t sbdf = vpci_sbdf_from_gpa(v->domain, bridge, info->gpa, false);
 
     ASSERT(!bridge == !is_hardware_domain(v->domain));
 
@@ -140,7 +145,7 @@ int domain_vpci_init(struct domain *d)
      * physical host bridge.
      * Guests get the virtual platform layout: one virtual host bridge for now.
      */
-    if ( is_hardware_domain(d) )
+    if ( !has_vpci_bridge(d) )
     {
         int ret;
 
@@ -155,8 +160,23 @@ int domain_vpci_init(struct domain *d)
             gdprintk(XENLOG_ERR, "vPCI requested but guest support not enabled\n");
             return -EINVAL;
         }
-        register_mmio_handler(d, &vpci_mmio_handler,
-                              GUEST_VPCI_ECAM_BASE, GUEST_VPCI_ECAM_SIZE, NULL);
+        if ( domain_use_host_layout(d) )
+        {
+            struct pci_host_bridge *bridge;
+
+            /* XXX: assume physical bridge is segment 0 bus 0 */
+            bridge = pci_find_host_bridge(0, 0);
+            if ( !bridge )
+                return 0;
+
+            register_mmio_handler(d, &vpci_mmio_handler,
+                                  bridge->cfg->phys_addr, bridge->cfg->size, bridge);
+        }
+        else
+        {
+            register_mmio_handler(d, &vpci_mmio_handler,
+                                  GUEST_VPCI_ECAM_BASE, GUEST_VPCI_ECAM_SIZE, NULL);
+        }
     }
 
     return 0;
@@ -178,7 +198,7 @@ unsigned int domain_vpci_get_num_mmio_handlers(struct domain *d)
     if ( !has_vpci(d) )
         return 0;
 
-    if ( is_hardware_domain(d) )
+    if ( !has_vpci_bridge(d) )
     {
         int ret = pci_host_iterate_bridges_and_count(d, vpci_get_num_handlers_cb);
 
@@ -206,4 +226,3 @@ unsigned int domain_vpci_get_num_mmio_handlers(struct domain *d)
  * indent-tabs-mode: nil
  * End:
  */
-
