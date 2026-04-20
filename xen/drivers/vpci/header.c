f2f95a02199b0dc357dd8cbac6ceaaaab299bd4b
Mykyta Poturai <mykyta_poturai@epam.com>
2026-04-20 17:47:42 +0300

    pci: Allow platforms to modify BAR adresses
    This patch is a preparatory work for adding Region ID support on Renesas
    R-Car series boards. Add new host bridge op "fixup_bar" that allows
    platforms to modify BAR addresses before they are mapped.
    
    Because x86 don't have support for PCI Host Bridge drivers, add another
    level of indirection in form of platform_pci_fixup_bar() function, that
    will call host bridge op on ARM and do nothing on x86.
    
    Signed-off-by: Mykyta Poturai <mykyta_poturai@epam.com>

--- xen/drivers/vpci/header.c
+++ xen/drivers/vpci/header.c
@@ -1038,6 +1038,8 @@ int vpci_init_header(struct pci_dev *pdev)
         bars[i].size = size;
         bars[i].prefetchable = val & PCI_BASE_ADDRESS_MEM_PREFETCH;
 
+        platform_pci_fixup_bar(pdev, i, &bars[i].addr);
+
         rc = vpci_add_register(pdev->vpci,
                                is_hwdom ? vpci_hw_read32
                                         : vpci_guest_mem_bar_read,
