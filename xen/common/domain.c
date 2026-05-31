979c1feddec4a5bbca74da3d71c203850da5fe94
Mykta Poturai <mykyta_poturai@epam.com>
2026-05-31 11:33:14 +0300

    [DOMD] vpci: Permit DomD access to BARs
    Signed-off-by: Mykta Poturai <mykyta_poturai@epam.com>

--- xen/common/domain.c
+++ xen/common/domain.c
@@ -989,18 +989,18 @@ struct domain *domain_create(domid_t domid,
          !IS_ENABLED(CONFIG_HAS_VPCI_GUEST_SUPPORT) )
         goto fail;
 
+    err = -ENOMEM;
+    d->iomem_caps = rangeset_new(d, "I/O Memory", RANGESETF_prettyprint_hex);
+    d->irq_caps   = rangeset_new(d, "Interrupts", 0);
+    if ( !d->iomem_caps || !d->irq_caps )
+        goto fail;
+
     if ( (err = arch_domain_create(d, config, flags)) != 0 )
         goto fail;
     init_status |= INIT_arch;
 
     watchdog_domain_init(d);
     init_status |= INIT_watchdog;
-
-    err = -ENOMEM;
-    d->iomem_caps = rangeset_new(d, "I/O Memory", RANGESETF_prettyprint_hex);
-    d->irq_caps   = rangeset_new(d, "Interrupts", 0);
-    if ( !d->iomem_caps || !d->irq_caps )
-        goto fail;
 
     if ( (err = xsm_domain_create(XSM_HOOK, d, config->ssidref)) != 0 )
         goto fail;
