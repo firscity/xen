7620766d4ff0f8e399e70dc0b25cac27b041ae88
Mykyta Poturai <mykyta_poturai@epam.com>
2026-06-08 17:58:49 +0300

    arm/pci: Fix VPCI init for non-dom0 pi host domain
    Move vPCI init to after the host bridges are assigned, so we can detect
    domain with PCI host bridges assigned correctly.
    
    Signed-off-by: Mykyta Poturai <mykyta_poturai@epam.com>

--- xen/arch/arm/domain.c
+++ xen/arch/arm/domain.c
@@ -752,9 +752,6 @@ int arch_domain_create(struct domain *d,
     if ( is_hardware_domain(d) && (rc = domain_vuart_init(d)) )
         goto fail;
 
-    if ( (rc = domain_vpci_init(d)) != 0 )
-        goto fail;
-
 #ifdef CONFIG_ARM64_SVE
     /* Copy the encoded vector length sve_vl from the domain configuration */
     d->arch.sve_vl = config->arch.sve_vl;
@@ -864,6 +861,8 @@ void arch_domain_creation_finished(struct domain *d)
         printk(XENLOG_INFO "Late vGIC initialization failed\n");
         domain_crash(d);
     }
+
+    ASSERT(!domain_vpci_init(d));
 
     p2m_domain_creation_finished(d);
 }
